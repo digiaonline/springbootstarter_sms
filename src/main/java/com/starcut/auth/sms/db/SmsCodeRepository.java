6bca949bb27c28fb56e9e1b09579e03d9ae08b10
Stephane Weiss <stephane.weiss@starcut.com>
2018-06-15 14:23:49 +0300

    Make the fetch last SmsCode lock a pessimistic write

--- src/main/java/com/starcut/auth/sms/db/SmsCodeRepository.java
+++ src/main/java/com/starcut/auth/sms/db/SmsCodeRepository.java
@@ -5,12 +5,16 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface SmsCodeRepository extends JpaRepository<SmsCode, Long> {
 
 	public Collection<SmsCode> findAllByPhonenumber(String phonenumber);
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	public List<SmsCode> findSmsCodeByPhonenumberAndCreatedAtGreaterThanOrderByCreatedAtDesc(String phonenumber,
 			Instant oldest);
 
