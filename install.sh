c1ad5b0e6f992c978fba3c280adea7cd92758a56
Stephane Weiss <stephane.weiss@starcut.com>
2018-08-06 15:30:23 +0300

    Add the install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mvn package && mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=target/sms-auth-spring-boot-starter-0.0.1-SNAPSHOT.jar.original -DpomFile=pom.xml
